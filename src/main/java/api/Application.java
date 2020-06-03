ff9a509b22960c769fb165b0154ccc948236d5f7
malinda <malindap@cbctechsol.com>
2020-06-03 20:24:47 +0530

    first commit

---
+++ src/main/java/api/Application.java
@@ -0,0 +1,16 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Project:   spring-swagger2markup
+ * @author CBC TECH 2020
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main  (String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
